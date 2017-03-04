58224a0bd421f8420fe4ea4fe6a1fb49cb89fa5a
rolin <rolin_B@hotmail.com>
2017-03-04 11:48:26 -0800

    logs

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,8 @@ var config  = require('../config.json');
 var express = require('express');
 var router  = express.Router();
 
-
+var width;
+var height;
 
 //test edit
 // Handle GET request to '/'
@@ -21,6 +22,10 @@ router.get(config.routes.info, function (req, res) {
 router.post(config.routes.start, function (req, res) {
   // Do something here to start the game
 
+  width = req.width;
+  height = req.height;
+
+
   // Response data
   var data = {
     taunt: config.snake.taunt.start
@@ -33,8 +38,16 @@ router.post(config.routes.start, function (req, res) {
 router.post(config.routes.move, function (req, res) {
   // Do something here to generate your move
 
-  console.log("test logging");
-  //console.log(req);
+  console.log("******************");
+  console.log("******************");
+  console.log("snake name maybe? " + req.you);
+  console.log("board height: " + height);
+  console.log("board width: " + width);
+
+  console.log("snakes?" + )
+
+  console.log("******************");
+  console.log("******************")
 
 
 
